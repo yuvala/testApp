aef2726c2892854575fc3da7f243658c8446f7aa
Your NameYuval <yuv.almaliah@gmail.com>
2021-12-02 00:04:42 +0200

    ...

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import {
+  Image,
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen b</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  aContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65',
+  },
+  registerButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fecd65',
+  },
+  logo: {
+    width: 70,
+    height: 70,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
+  },
+});
+export default HomeScreen;
